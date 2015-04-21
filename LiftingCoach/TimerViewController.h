3eb642035bd7f78d50336134306a8b5cb36b41bc
jvasq3 <juanvasqueziii@gmail.com>
2015-04-20 19:00:43 -0500

    timer

---
+++ LiftingCoach/TimerViewController.h
@@ -0,0 +1,14 @@
+//
+//  TimerViewController.h
+//  LiftingCoach
+//
+//  Created by Juan Vasquez on 4/17/15.
+//  Copyright (c) 2015 CS378. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimerViewController : UIViewController
+
+
+@end
