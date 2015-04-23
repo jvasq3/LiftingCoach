fc8166dc1a076fa2884d92a96d114f4680d7c4f2
mwmardis <Michael Mardis>
2015-04-22 20:40:37 -0500

    changed sets to reps in workout.m

--- LiftingCoach/Workout.m
+++ LiftingCoach/Workout.m
@@ -24,8 +24,8 @@
     if (self) {
         _day = dayA;
         
-        Exercise *exStrongLiftA1 = [[Exercise alloc]initWithName:squat targetSets:fiveReps targetReps:fiveSets];
-        Exercise *exStrongLiftA2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveReps targetReps:fiveSets];
+        Exercise *exStrongLiftA1 = [[Exercise alloc]initWithName:squat targetSets:fiveSets targetReps:fiveReps];
+        Exercise *exStrongLiftA2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveSets targetReps:fiveReps];
         Exercise *exStrongLiftA3 = [[Exercise alloc]initWithName:barbellRow targetSets:fiveSets targetReps:fiveReps];
         _exercises = [NSMutableArray arrayWithObjects:exStrongLiftA1, exStrongLiftA2, exStrongLiftA3, nil];
         
@@ -38,9 +38,9 @@
     if (self) {
         _day = dayB;
         
-        Exercise *exStrongLiftB1 = [[Exercise alloc]initWithName:squat targetSets:fiveReps targetReps:fiveSets];
-        Exercise *exStrongLiftB2 = [[Exercise alloc]initWithName:overhead targetSets:fiveReps targetReps:fiveSets];
-        Exercise *exStrongLiftB3 = [[Exercise alloc]initWithName:deadlift targetSets:oneSet targetReps:fiveSets];
+        Exercise *exStrongLiftB1 = [[Exercise alloc]initWithName:squat targetSets:fiveSets targetReps:fiveReps];
+        Exercise *exStrongLiftB2 = [[Exercise alloc]initWithName:overhead targetSets:fiveSets targetReps:fiveReps];
+        Exercise *exStrongLiftB3 = [[Exercise alloc]initWithName:deadlift targetSets:oneSet targetReps:fiveReps];
         _exercises = [NSMutableArray arrayWithObjects:exStrongLiftB1, exStrongLiftB2, exStrongLiftB3, nil];
         
     }
@@ -52,9 +52,9 @@
     if (self) {
         _day = dayC;
         
-        Exercise *exStrongLiftC1 = [[Exercise alloc]initWithName:squat targetSets:fiveReps targetReps:fiveSets];
-        Exercise *exStrongLiftC2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveReps targetReps:fiveSets];
-        Exercise *exStrongLiftC3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveSets];
+        Exercise *exStrongLiftC1 = [[Exercise alloc]initWithName:squat targetSets:fiveSets targetReps:fiveReps];
+        Exercise *exStrongLiftC2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveSets targetReps:fiveReps];
+        Exercise *exStrongLiftC3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:fiveSets targetReps:fiveReps];
         _exercises = [NSMutableArray arrayWithObjects:exStrongLiftC1, exStrongLiftC2, exStrongLiftC3, nil];
         
     }
@@ -68,9 +68,9 @@
     self = [super init];
     if (self) {
         _day = dayA;
-        Exercise *wendlersA1 = [[Exercise alloc]initWithName:squat targetSets:fiveReps targetReps:fiveSets];
-        Exercise *wendlersA2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveReps targetReps:fiveSets];
-        Exercise *wendlersA3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveSets];
+        Exercise *wendlersA1 = [[Exercise alloc]initWithName:squat targetSets:fiveSets targetReps:fiveReps];
+        Exercise *wendlersA2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveSets targetReps:fiveReps];
+        Exercise *wendlersA3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:fiveReps targetReps:fiveSets];
         _exercises = [NSMutableArray arrayWithObjects:wendlersA1, wendlersA2, wendlersA3, nil];
         
     }
@@ -81,9 +81,9 @@
     self = [super init];
     if (self) {
         _day = dayB;
-        Exercise *wendlersB1 = [[Exercise alloc]initWithName:benchPress targetSets:fiveReps targetReps:fiveSets];
-        Exercise *wendlersB2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveReps targetReps:fiveSets];
-        Exercise *wendlersB3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveSets];
+        Exercise *wendlersB1 = [[Exercise alloc]initWithName:benchPress targetSets:fiveSets targetReps:fiveReps];
+        Exercise *wendlersB2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveSets targetReps:fiveReps];
+        Exercise *wendlersB3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveReps];
         _exercises = [NSMutableArray arrayWithObjects:wendlersB1, wendlersB2, wendlersB3, nil];
         
     }
@@ -94,9 +94,9 @@
     self = [super init];
     if (self) {
         _day = dayC;
-        Exercise *wendlersC1 = [[Exercise alloc]initWithName:deadlift targetSets:fiveReps targetReps:fiveSets];
-        Exercise *wendlersC2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveReps targetReps:fiveSets];
-        Exercise *wendlersC3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveSets];
+        Exercise *wendlersC1 = [[Exercise alloc]initWithName:deadlift targetSets:fiveSets targetReps:fiveReps];
+        Exercise *wendlersC2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveSets targetReps:fiveReps];
+        Exercise *wendlersC3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveReps];
         _exercises = [NSMutableArray arrayWithObjects:wendlersC1, wendlersC2, wendlersC3, nil];
         
     }
@@ -107,9 +107,9 @@
     self = [super init];
     if (self) {
         _day = dayD;
-        Exercise *wendlersD1 = [[Exercise alloc]initWithName:overhead targetSets:fiveReps targetReps:fiveSets];
-        Exercise *wendlersD2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveReps targetReps:fiveSets];
-        Exercise *wendlersD3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveSets];
+        Exercise *wendlersD1 = [[Exercise alloc]initWithName:overhead targetSets:fiveSets targetReps:fiveReps];
+        Exercise *wendlersD2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveSets targetReps:fiveReps];
+        Exercise *wendlersD3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveReps];
         _exercises = [NSMutableArray arrayWithObjects:wendlersD1, wendlersD2, wendlersD3, nil];
         
     }
@@ -123,9 +123,9 @@
     self = [super init];
     if (self) {
         _day = dayA;
-        Exercise *madCowsA1 = [[Exercise alloc]initWithName:squat targetSets:fiveReps targetReps:fiveSets];
-        Exercise *madCowsA2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveReps targetReps:fiveSets];
-        Exercise *madCowsA3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveSets];
+        Exercise *madCowsA1 = [[Exercise alloc]initWithName:squat targetSets:fiveSets targetReps:fiveReps];
+        Exercise *madCowsA2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveSets targetReps:fiveReps];
+        Exercise *madCowsA3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveReps];
         _exercises = [NSMutableArray arrayWithObjects:madCowsA1, madCowsA2, madCowsA3, nil];
         
     }
@@ -136,9 +136,9 @@
     self = [super init];
     if (self) {
         _day = dayB;
-        Exercise *madCowsB1 = [[Exercise alloc]initWithName:squat targetSets:fiveReps targetReps:fiveSets];
-        Exercise *madCowsB2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveReps targetReps:fiveSets];
-        Exercise *madCowsB3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveSets];
+        Exercise *madCowsB1 = [[Exercise alloc]initWithName:squat targetSets:fiveSets targetReps:fiveReps];
+        Exercise *madCowsB2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveSets targetReps:fiveReps];
+        Exercise *madCowsB3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveReps];
         _exercises = [NSMutableArray arrayWithObjects:madCowsB1, madCowsB2, madCowsB3, nil];
         
     }
@@ -149,9 +149,9 @@
     self = [super init];
     if (self) {
         _day = dayC;
-        Exercise *madCowsC1 = [[Exercise alloc]initWithName:squat targetSets:fiveReps targetReps:fiveSets];
-        Exercise *madCowsC2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveReps targetReps:fiveSets];
-        Exercise *madCowsC3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveSets];
+        Exercise *madCowsC1 = [[Exercise alloc]initWithName:squat targetSets:fiveSets targetReps:fiveReps];
+        Exercise *madCowsC2 = [[Exercise alloc]initWithName:benchPress targetSets:fiveSets targetReps:fiveReps];
+        Exercise *madCowsC3 = [[Exercise alloc]initWithName:dumbbellRow targetSets:oneSet targetReps:fiveReps];
         _exercises = [NSMutableArray arrayWithObjects:madCowsC1, madCowsC2, madCowsC3, nil];
         
     }
